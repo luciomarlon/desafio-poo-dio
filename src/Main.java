4fc9a6f29692afd195bfc281ccb768af1e8401aa
luciomarlon <luciomarlon@gmail.com>
2022-01-30 10:43:39 -0300

    Criando Mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import com.br.com.dio.desafio.dominio.Curso;
+import com.br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+        curso.setTitulo("TÍTULO: Curso JAVA");
+        curso.setDescricao("DESCRIÇÃO: curso java");
+        curso.setCargaHoraria(8);
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("TÍTULO: Curso JavaScript");
+        curso1.setDescricao("DESCRIÇÃO: curso javascript");
+        curso1.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria Java");
+        mentoria.setDescricao("Descrição mentoria JAVA");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso1);
+        System.out.println(mentoria);
+    }
+}
